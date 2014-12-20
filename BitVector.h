8150551e2592a902dc90bd9375a20f40bee46d41
Austin Herring <awh44@drexel.edu>
2014-12-20 00:27:40 -0500

    Improved comments in header file.

--- BitVector.h
+++ BitVector.h
@@ -17,23 +17,65 @@ typedef struct
 	size_t units;
 } BitVector;
 
-//initialize the BitVector to hold num_bits number of bits
+/*
+	initializes the BitVector to handle the given number of elements, num_bits; should be called
+	before using the BitVector
+	@param bv the BitVector to initialize
+	@param num_bits the number of bits the BitVector is to be able to handle
+*/
 void bitvector_initialize(BitVector *bv, size_t num_bits);
-//free the BitVector
+
+/*
+	handles unitializing the BitVector; should be called after the BitVector is done with
+	@param bv the BitVector to uninitialize
+*/
 void bitvector_uninitialize(BitVector *bv);
-//invert every bit in the BitVector bv
+
+/*
+	invert every bit in the BitVector bv; every 1 goes to 0, every 0 goes to 1
+	@param bv the BitVector whose bits are to be inverted
+*/
 void bitvector_invert_all_bits(BitVector *bv);
-//set every the BitVector to 1
+
+/*
+	set every bit in the BitVector to 1
+	@param bv the BitVector whose bits are to be set to 1
+*/
 void bitvector_set_all_bits(BitVector *bv);
-//zero the BitVector
+
+/*
+	zeros the BitVector, setting every bit to 0
+	@param bv the BitVector whose bits are to be set to 0
+*/
 void bitvector_unset_all_bits(BitVector *bv);
-//set the bit at bit_num (with zero-indexing) to 'true' (i.e., 1)
+
+/*
+	set the bit at bit_num to 1
+	@param bv the BitVector which is to have a bit set
+	@param bit_num the particular bit which is to be set
+*/
 void bitvector_set_bit(BitVector *bv, size_t bit_num);
-//set the bit at bit_num  to 'false' (i.e., 0)
+
+/*
+	set the bit at bit_num to 0
+	@param bv the BitVector which is to have a bit unset
+	@param bit_num the particular bit which is to be unset
+*/
 void bitvector_unset_bit(BitVector *bv, size_t bit_num);
-//toggle the bit in the given position - i.e, 1 goes to 0, 0 to 1
+
+/*
+	toggle the bit in the given position - if 1, set to 0; if 0, set to 1
+	@param bv the BitVector which is to have a bit inverted
+	@param bit_num the particular bit which is to be inverted
+*/
 void bitvector_invert_bit(BitVector *bv, size_t bit_num);
-//get the truth value of the bit at bit_num, either 0 or 1
+
+/*
+	get the truth value of the bit at bit_num, either 0 or 1
+	@param bv the BitVector from which to get the value
+	@param bit_num the particular bit which is to be gotten
+	@return the truth value of bit bit_num in bv, either 0 or 1
+*/
 unsigned short bitvector_get_bit(BitVector *bv, size_t bit_num);
 
 #endif
